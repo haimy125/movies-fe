91b4505aefeb3eb7d8e2d13bd731416e55b09ba1
myphan <haimyphan125@gmail.com>
2024-12-16 16:25:50 +0700

    fix some bug

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,7 @@ const useAuth = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(null); // Đặt là null để xác định trạng thái chờ
   const [user, setUser] = useState(null);
   const token = Cookies.get('token');
-
+  console.log("token test:::",token);
   useEffect(() => {
     const fetchUserInfoFromApi = async () => {
       try {
@@ -30,3 +30,47 @@ const useAuth = () => {
 };
 
 export { useAuth };
+
+// import axios from 'axios';
+
+// const authService = {
+//   getAccessToken: () => localStorage.getItem('accessToken'),
+//   getRefreshToken: () => localStorage.getItem('refreshToken'),
+//   setAccessToken: (token) => localStorage.setItem('accessToken', token),
+//   setRefreshToken: (token) => localStorage.setItem('refreshToken', token),
+
+//   refreshAccessToken: async () => {
+//     const refreshToken = authService.getRefreshToken();
+//     if (!refreshToken) throw new Error('No refresh token available');
+
+//     const response = await axios.post('/api/refresh-token', { refreshToken });
+//     const { accessToken } = response.data;
+
+//     authService.setAccessToken(accessToken);
+//     return accessToken;
+//   },
+// };
+
+// axios.interceptors.request.use(
+//   async (config) => {
+//     let accessToken = authService.getAccessToken();
+//     config.headers.Authorization = `Bearer ${accessToken}`;
+
+//     // Kiểm tra token hết hạn
+//     const isTokenExpired = false; // Thêm logic kiểm tra expiration của token
+//     if (isTokenExpired) {
+//       try {
+//         accessToken = await authService.refreshAccessToken();
+//         config.headers.Authorization = `Bearer ${accessToken}`;
+//       } catch (error) {
+//         console.error('Token refresh failed', error);
+//         // Redirect nếu cần
+//       }
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default authService;
