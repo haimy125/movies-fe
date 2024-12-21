38c60f3fc2a70402877ffdc5a5f441457633d16b
Giang Do <giangz0009@gmail.com>
2024-12-21 18:34:40 +0700

    .

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import './MovieCard.css'; // Tạo file CSS cho styling
+import React from "react";
+import "./MovieCard.css"; // Tạo file CSS cho styling
 
-const MovieCard = ({ id,name,vip,ep }) => {
+const MovieCard = ({ id, name, vip, ep }) => {
   const truncateText = (text, maxLength) => {
     if (text && text?.length > maxLength) {
-      return text?.slice(0, maxLength) + '...';
+      return text?.slice(0, maxLength) + "...";
     }
     return text;
   };
   return (
     <div className="movie-card">
-      <div className='movie_card_item'>
-        <img src={`http://localhost:1412/user/home/view/${id}`} alt={name} className="movie-image" />
-        <div className='name_card_item'>
-          <h3>{truncateText(name,35)}</h3>
+      <div className="movie_card_item">
+        <img
+          src={`http://localhost:1412/user/home/view/${id}`}
+          alt={name}
+          className="movie-image"
+        />
+        <div className="name_card_item">
+          <h3>{truncateText(name, 35)}</h3>
         </div>
-        <div className='ep_card_item'>
+        <div className="ep_card_item">
           <span>{ep}</span>
         </div>
-        <div className='vip_card_item'>
-          {vip===true ?<span>VIP</span> : '' }
-          
+        <div className="vip_card_item">
+          {vip === true ? <span>VIP</span> : ""}
         </div>
       </div>
     </div>
